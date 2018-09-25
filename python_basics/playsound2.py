1f21023eab86c65fb07ecc604177f21172eca4a9
Sander <sanderkesterm@hotmail.com>
2018-09-25 11:46:04 +0200

    Verbetering van 1 sample afspelen (RitmischPlayback)

--- python_basics/playsound2.py
+++ python_basics/playsound2.py
@@ -1,35 +1,38 @@
+#Alleen 16bit files
+
 import simpleaudio as sa
 import time
 
+#ImportFiles
+wave_obj = sa.WaveObject.from_wave_file("/home/sander/documentos/Samples/hihat2.wav")
+
 #Define Bpm
-print("Insert bpm")
+print("Welcome to the sandersampler")
+print("Insert bpm and 1 file will be played")
 bpm = int(input())
 bpmsec = 60 / bpm
 print("Bpm is ", bpmsec, "sec")
 
-#Make list array
-#Defineer list van 4 noot lengtes
-print("Insert 4 notelenghts")
+#Make list array / Defineer list van 4 noot lengtes
+print("Insert 4 notelenghts and press enter after eachone")
 list1 = []
 for maatnoot in range(0, 4):
     maatnoot = float(input())
     list1.append(maatnoot)
-#Kies welke getal in de lijst
-#print(list1[0])
+
+#print(len(list1))
 
 #Defineer aantal van afspelen
 print("Insert times to repeat sample")
 aantalplays = int(input())
 print("Sample will be repeated", aantalplays, "times")
 
+#amountofplays = aantalplays
 
-amountofplays = int(input())
-amountofplays = aantalplays
-
-for x in range(0, amountofplays):
-    for x in range(0, aantalplays):
-        wave_obj = sa.WaveObject.from_wave_file("/home/sander/documentos/Samples/hihat2.wav")
+#Loop play and read func as many times as user chooses to be played (> 4)
+for i in range(0, aantalplays):
+    #play and read sample x times
+    for x in range(0, 4):
         play_obj = wave_obj.play()
- #   play_obj.wait_done()
+        #   play_obj.wait_done()
         time.sleep(bpmsec*list1[x])
-        print(x)
